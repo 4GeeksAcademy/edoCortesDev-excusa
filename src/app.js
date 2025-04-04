1a42b353991a2986d803e3b1bb8c6673218efd08
edoCortesDev <edo.cortes.rojas@gmail.com>
2025-04-04 13:24:36 +0000

    Modificación de los documentos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  // array with the words
+  let inicio = "Un ";
+  let adjetivos = ["enorme ", "pegajoso ", "eléctrico ", "sorprendente ", "invisible ", "saltón ", "gritón ", "despistado ", "brillante "];
+  let sujetos = ["payaso ", "ornitorrinco ", "robot ", "niño ", "abuelo ", "fantasma ", "zombi ", "mapache "];
+  let acciones = ["lanzó mi ", "escondió mi ", "rompió mi ", "pintó mi ", "gritó sobre mi ", "mojó mi ", "secuestró mi "];
+  let objetos = ["bocina ", "cepillo de dientes ", "torta ", "guitarra ", "cargador ", "calcetín ", "cuaderno ", "hamburguesa "];
+  let lugares = ["en la azotea", "en el ascensor", "debajo del sofá", "en el supermercado", "cerca de la estación", "detrás del baño", "en la panadería"];
+
+  // declaring random variables
+  let r1 = Math.floor(Math.random() * adjetivos.length);
+  let r2 = Math.floor(Math.random() * sujetos.length);
+  let r3 = Math.floor(Math.random() * acciones.length);
+  let r4 = Math.floor(Math.random() * objetos.length);
+  let r5 = Math.floor(Math.random() * lugares.length);
+
+  // creating a sentence (the excuse)
+  document.querySelector("#excuse").innerHTML = inicio + adjetivos[r1] + sujetos[r2] + acciones[r3] + objetos[r4] + lugares[r5];
+}
